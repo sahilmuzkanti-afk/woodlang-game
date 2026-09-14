6f927c9ee758851dbc0746fffeeaed09ba840e9b
Ghost-af16 <virajmuz16@gmail.com>
2026-09-14 16:41:53 +0530

    Added sprite and collision base

--- script.js
+++ script.js
@@ -27,3 +27,101 @@ const scaledCanvas = {
     width: canvas.width / BACKGROUND_SCALE,
     height: canvas.height / BACKGROUND_SCALE
 }
+class Sprite {
+    constructor({position, imageSrc, scale=1, numFrames=1, animationSpeed = ANIMATION_SPEED}) {
+        this.position = position;
+        this.image=new Image();
+        this.image.src = imageSrc
+        this.loaded=false
+        this.scale=scale
+        this.numFrames = numFrames
+        this.currentFrame = 0
+        this.elapsedFrames = 0
+        this.animationSpeed = animationSpeed
+        this.image.onload = () => {
+            this.loaded = true
+            this.width= ( this.image.width/this.numFrames ) * this.scale
+            this.height = (this.image.height ) * this.scale
+        }
+    }
+}
+Sprite.prototype.draw = function() {
+    if (!this.loaded)
+        return;
+    else {
+        canvasContext.drawImage(
+            this.image,
+            this.currentFrame * (this.image.width / this.numFrames),
+            0,
+            this.image.width / this.numFrames,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            this.width ,
+            this.height
+        )
+    }
+}
+Sprite.prototype.update = function() {
+    this.draw();
+    this.animate();
+}
+Sprite.prototype.animate = function() {
+    this.elapsedFrames++
+    if(this.elapsedFrames % this.animationSpeed === 0 ) {
+        if(this.currentFrame < (this.numFrames-1)) {
+            this.currentFrame++;
+        } else {
+            this.currentFrame = 0;
+        }
+    }
+}
+class CollissionBlock {
+    constructor({position, height=TILE_DIM}) {
+        this.position = position;
+        this.width= TILE_DIM;
+        this.height=height
+    }
+    draw() {
+    }
+    update() {
+        this.draw();
+    }
+}
+function detectCollission({ obj1, obj2 }) {
+    if (obj1.position.y + obj1.height >= obj2.position.y
+        &&
+        obj1.position.y <= obj2.position.y + obj2.height
+        &&
+        obj1.position.x <= obj2.position.x + obj2.width
+        &&
+        obj1.position.x + obj1.width >= obj2.position.x
+    ) { return true } else { return false }
+}
+function platformCollission({ obj1, obj2 }) {
+    if (obj1.position.y + obj1.height >= obj2.position.y
+        &&
+        obj1.position.y + obj1.height <= obj2.position.y + obj2.height
+        &&
+        obj1.position.x <= obj2.position.x + obj2.width
+        &&
+        obj1.position.x + obj1.width >= obj2.position.x
+    ) { return true } else { return false }
+}
+function checkForHorizontalCollissions(object) {
+    for(let i=0; i< currentLevel.collissionBlocksArray.length; i++) {
+        const currentBlock = currentLevel.collissionBlocksArray[i]
+        if ( detectCollission({ obj1: object, obj2: currentBlock}) ) {
+            if(object.velocity.x > 0) {
+                object.velocity.x = 0;
+                object.position.x = currentBlock.position.x - object.width -0.02
+                break
+            }
+            if(object.velocity.x<0) {
+                object.velocity.x=0;
+                object.position.x = currentBlock.position.x + currentBlock.width + 0.02
+                break
+            }
+        }
+    }
+}
